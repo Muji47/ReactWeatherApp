d17a25642f0f63243eedc2cb823bb66d6a0febaf
Muhammad Mujtaba Tahir <97501928+Muji47@users.noreply.github.com>
2024-02-21 12:20:11 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   const [inputCity,setInputCity]=useState('')
   return (
     <div className='flex justify-center items-center flex-col h-screen text-center flex-wrap'>
-        <h1 className='font-bold text-4xl text-[#29ADB2] w-72 '>Welcome to our website</h1>
+        <h1 className='font-bold text-4xl text-[#29ADB2] max-w-72 '>Welcome to our website</h1>
         <p className='text-2xl w-[29rem]'>Here you can find the weather or situation of any city.</p>
         <div className='bg-[#29ADB2] p-2 my-1'>
         <input type='text' placeholder='Add city' value={inputCity} onChange={(e)=>setInputCity(e.target.value)} className='px-3 py-1 rounded-3xl text-gray-600 w-[25rem] outline-none'/> 
