8eea8aaaeb6b0e0d702ba2822b05ccb4e92fc341
Muji47 <mujtabach577@gmail.com>
2024-02-21 11:01:39 +0500

    New Commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import City from './pages/City';
-import { useState } from 'react';
 
 
 
 
 function App() {
-const [inputCity,setInputCity]=useState('')
-const [city,setCity]=useState('')
 
-
-console.log(inputCity,"sdgdfs",city)
   return (
     <div>
       <BrowserRouter>
       <Routes>
-          <Route exact path='/' element={<Home inputvalue={inputCity} setInput={setInputCity} />}/>
+          <Route exact path='/' element={<Home />}/>
           <Route exact path=":weather" element={<City />} />
       </Routes>
     </BrowserRouter>
